9d183981808bbfa8521b5957b98655c125392102
Mateus Rodrigues Da Fonseca <46423117+MateusRodriguesF@users.noreply.github.com>
2021-12-22 11:48:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+//Função para abrir o link em uma nova janela
+function openWindow() {
+  window.open("http://algumsite.com", "My Window Name", "width=400,height=400");
+}
+
+function openWindow2() {
+  window.open("http://algumsite.com", "My Window Name", "width=400,height=400");
+}
+// Create a "close" button and append it to each list item
+// Cria um botão "close (fechar)" e acrescenta a cada item da lista.
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+// Clique em um botão "close (fechar)" para esconder o item atual.
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+// Add a "checked" symbol when clicking on a list item
+// Adiciona um simbolo de verificado(checked) quando clicar sobre um item na lista.
+
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
+
+// Create a new list item when clicking on the "Add" button
+// Cria um novo item na lista quando clicar no botão adicionar(add).
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("Escreva alguma coisa!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
